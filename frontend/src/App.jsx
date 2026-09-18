8425c355b035ea8426c2c0f5b5d96de5a9ff9a66
unknown <shreyathakurb@gmail.com>
2026-09-18 23:36:15 +0530

    Add headerfix file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import CreateTicket from "./pages/CreateTicket";
 import TicketDetail from "./pages/TicketDetail";
